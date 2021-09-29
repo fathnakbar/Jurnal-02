dd45d1982e996da8ecc8e1075e54e65be4e4cad6
Muhammad Fathan Akbar <fathnakbar@gmail.com>
2021-09-29 19:16:52 +0700

    Hello

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Fathan Akbar");
+        System.out.println("NIM: 6706213071");
+        System.out.println("Kelas: 45-01");
+    }
+}
